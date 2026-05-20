2cf58020c4125a9bf3043cff14255b72245e4f21
amixrzi <amixrzie@gmail.com>
2026-05-20 14:35:57 +0330

    Implement values and variables

---
+++ 01-Fundamentals-Part-1/01-values-variables.js
@@ -0,0 +1,11 @@
+// Learning: console.log, variables, naming conventions
+console.log(40 + 11 - 2 + 6);
+console.log("amixrzi");
+console.log(23);
+
+let firstName = "Amir";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415926535;
