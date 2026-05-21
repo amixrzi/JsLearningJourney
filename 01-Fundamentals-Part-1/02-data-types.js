f56ab36dcf21059178aee4e8eb686e1179d89a97
amixrzi <amixrzie@gmail.com>
2026-05-21 22:39:21 +0330

    Implement data types

---
+++ 01-Fundamentals-Part-1/02-data-types.js
@@ -0,0 +1,18 @@
+// Data Types – typeof, dynamic typing, null bug
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Amir");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1989;
+console.log(typeof year);
+console.log(typeof null);
