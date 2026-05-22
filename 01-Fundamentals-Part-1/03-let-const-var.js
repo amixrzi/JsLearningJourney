8e50e60ff0d98a7e7e996b27a87b8d19e81daeb2
amixrzi <amixrzie@gmail.com>
2026-05-22 18:30:59 +0330

    Implement let, const and var

---
+++ 01-Fundamentals-Part-1/03-let-const-var.js
@@ -0,0 +1,13 @@
+// let, const and var -- variable declaration
+let age = 18;
+age = 19;
+
+const birthYear = 2007;
+// birthYear = 2009;
+// const job;
+
+var jop = "Programmer";
+job = "student";
+
+firstName = "Amir";
+console.log(firstName);
