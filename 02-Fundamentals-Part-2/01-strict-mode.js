665f786d348dea00b36ea769a022dc4937db9bcc
amixrzi <amixrzie@gmail.com>
2026-06-08 21:35:59 +0330

    Implement strict mode

---
+++ 02-Fundamentals-Part-2/01-strict-mode.js
@@ -0,0 +1,12 @@
+// Strict Mode ("use strict")
+
+'use strict'
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
