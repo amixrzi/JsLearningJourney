eb31ab8e34d3dd1830ce26c41c598ea94c801ca6
amixrzi <amixrzie@gmail.com>
2026-06-13 21:04:31 +0330

    Implement objects

---
+++ 02-Fundamentals-Part-2/11-objects.js
@@ -0,0 +1,18 @@
+// Objects
+'use strict'
+
+const amirArray = [
+    'Amirhossein',
+    'Rezaee',
+    2026 - 2007,
+    'student',
+    ['Nima', 'Matin', 'Alireza']
+];
+
+const amir = {
+    firstName: 'Amir',
+    lastName: 'Rezaee',
+    age: 2026 - 2007,
+    job: 'student',
+    friends: ['Nima', 'Matin', 'Alireza']
+}
