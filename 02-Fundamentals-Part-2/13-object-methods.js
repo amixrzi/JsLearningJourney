f28879ccdca14262082efe7111588df295979bd1
amixrzi <amixrzie@gmail.com>
2026-06-15 22:59:38 +0330

    Implement object methods

---
+++ 02-Fundamentals-Part-2/13-object-methods.js
@@ -0,0 +1,35 @@
+// Object Methods
+'use strict'
+
+const amir = {
+    firstName: 'Amir',
+    lastName: 'Rezaee',
+    birthYeah: 2007,
+    job: 'student',
+    friends: ['Nima', 'Matin', 'Alireza'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYeah) {
+    //   return 2037 - birthYeah;
+    // }
+
+    // calcAge: function () {
+    //   // console.log(this);
+    //   return 2037 - this.birthYeah;
+    // }
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYeah;
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${amir.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+
+console.log(amir.calcAge());
+
+console.log(amir.age);
+console.log(amir.age);
+console.log(amir.age);
