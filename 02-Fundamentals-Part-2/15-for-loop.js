6f7b24ecf3bdf3962d72ecc8346fda57404ca4ba
amixrzi <amixrzie@gmail.com>
2026-07-17 22:17:46 +0330

    Implement for loop

---
+++ 02-Fundamentals-Part-2/15-for-loop.js
@@ -0,0 +1,17 @@
+'use strict'
+// The For Loop
+
+// console.log('Lifting wights repetition 1 🏋️‍♂️')
+// console.log('Lifting wights repetition 2 🏋️‍♂️');
+// console.log('Lifting wights repetition 3 🏋️‍♂️');
+// console.log('Lifting wights repetition 4 🏋️‍♂️');
+// console.log('Lifting wights repetition 5 🏋️‍♂️');
+// console.log('Lifting wights repetition 6 🏋️‍♂️');
+// console.log('Lifting wights repetition 7 🏋️‍♂️');
+// console.log('Lifting wights repetition 8 🏋️‍♂️');
+// console.log('Lifting wights repetition 9 🏋️‍♂️');
+// console.log('Lifting wights repetition 10 🏋️‍♂️');
+
+for (let rep = 1 ; rep <= 10; rep++) {
+    console.log(`Lifting wights repetition ${rep} 🏋️‍`);
+}
