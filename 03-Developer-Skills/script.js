b95156d7c85c5e2ca4c19653a807db1dac19b93f
amixrzi <amixrzie@gmail.com>
2026-07-21 23:24:51 +0330

    Implement coding challenge 1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -69,43 +69,60 @@
 ///////////////////////////////////////
 // Debugging with Console and Breakpoints
 
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temp',
-    unit: 'celsius',
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//
+//     // C) FIX
+//     // value: Number(prompt('Developer Celsius')),
+//     value: 10,
+//   };
+//
+//   // B) FIND
+//   console.table(measurement);
+//   // console.log(measurement);
+//   // console.warn(measurement);
+//   // console.error(measurement);
+//
+//   const kelvin = measurement.value + 10;
+//   return kelvin;
+// };
+// console.log(measureKelvin());
+//
+// // Using a Debugger
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   let max = 0;
+//   let min = 0;
+//
+//   for (let i = 1; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeBug([1, 5, 7], [12, 4, 3]);
+// // A) IDENTIFY
+// console.log(amplitudeNew);
 
-    // C) FIX
-    // value: Number(prompt('Developer Celsius')),
-    value: 10,
-  };
+///////////////////////////////////////
+// Coding Challenge #1
+// Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ...";
 
-  // B) FIND
-  console.table(measurement);
-  // console.log(measurement);
-  // console.warn(measurement);
-  // console.error(measurement);
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
-  const kelvin = measurement.value + 10;
-  return kelvin;
-};
-console.log(measureKelvin());
-
-// Using a Debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  let max = 0;
-  let min = 0;
-
-  for (let i = 1; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 1; i < arr.length; i++) {
+    str += `${arr[i - 1]}ºC ins ${i} days ... `;
   }
-  console.log(max, min);
-  return max - min;
+  console.log('...', str);
 };
-const amplitudeNew = calcTempAmplitudeBug([1, 5, 7], [12, 4, 3]);
-// A) IDENTIFY
-console.log(amplitudeNew);
+printForecast(data1);
+printForecast(data2);
