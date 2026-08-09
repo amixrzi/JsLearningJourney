1872c6532ed85af70b29d4ad2a6d307e1859c4aa
amixrzi <amixrzie@gmail.com>
2026-08-09 23:15:05 +0330

    Implement selecting and manipulating elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
-document.querySelector('.message');
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!🎉';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 19;
+document.querySelector('.score').textContent = 20;
+
+console.log((document.querySelector('.guess').value = 32));
+console.log(document.querySelector('.guess').value);
