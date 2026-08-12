b2a03443d10f26b175de1226f6c1ee7851401667
amixrzi <amixrzie@gmail.com>
2026-08-13 01:15:59 +0330

    Implement handling click events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!🎉';
 console.log(document.querySelector('.message').textContent);
@@ -9,3 +9,13 @@ document.querySelector('.score').textContent = 20;
 
 console.log((document.querySelector('.guess').value = 32));
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔⛔ pls enter a number';
+  }
+});
