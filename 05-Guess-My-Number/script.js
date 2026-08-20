85b5e6602ab1709b27cbfe1cb54e4e6c9a14ecc0
amixrzi <amixrzie@gmail.com>
2026-08-20 22:36:47 +0330

    Implement challenge 01

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,13 +10,12 @@ document.querySelector('.score').textContent = 20;
 console.log((document.querySelector('.guess').value = 32));
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // when there is ni input
@@ -25,7 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!🎉';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -50,4 +49,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
 });
