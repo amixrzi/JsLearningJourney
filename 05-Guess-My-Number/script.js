13065f6d5538217c34afc7378ddbedded8ab438b
amixrzi <amixrzie@gmail.com>
2026-08-20 21:52:11 +0330

    Implement game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,6 +10,10 @@ document.querySelector('.score').textContent = 20;
 console.log((document.querySelector('.guess').value = 32));
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20 + 2);
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,5 +21,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔⛔ pls enter a number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!😒';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!😒';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
