31dc4f8aebef56694f3d8688e73bb130f1d82861
amixrzi <amixrzie@gmail.com>
2026-08-24 23:39:00 +0330

    Implement DOM element selection

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+console.log(btnCloseModel);
+
+for (let i = 0; i < btnOpenModel; i++) {
+  console.log(btnOpenModel[i].textContent);
+}
