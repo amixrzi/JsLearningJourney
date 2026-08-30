44a466581d2c3056313c4488bb316fc68aa8dc61
amixrzi <amixrzie@gmail.com>
2026-08-30 21:37:32 +0330

    Implement refactoring code

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -50,7 +50,7 @@ btnRoll.addEventListener('click', function () {
 
     // 2. display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `./images/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
