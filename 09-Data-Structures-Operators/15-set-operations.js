1540fcf70cba535b995c88dda85311bc3e0e100d
amixrzi <amixrzie@gmail.com>
2026-09-16 15:55:07 +0330

    Implement set operations

---
+++ 09-Data-Structures-Operators/15-set-operations.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('intersection:', commonFoods);
+console.log([...commonFoods]);
+
+const italianMexicanFusion = italianFoods.union(mexicanFoods);
+console.log('Union:', italianMexicanFusion);
+
+console.log([...new Set([...italianFoods, ...mexicanFoods])]);
+
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+console.log('Difference italian:', uniqueItalianFoods);
+
+const uniqueMexicanFoods = mexicanFoods.difference(italianFoods);
+console.log('Difference mexican:', uniqueItalianFoods);
+
+const uniqueMexicanAndItalianFoods =
+  italianFoods.symmetricDifference(mexicanFoods);
+console.log(uniqueMexicanAndItalianFoods);
+
+console.log(italianFoods.isDisjointFrom(mexicanFoods));
