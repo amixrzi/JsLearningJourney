3f15592966b7a099a4136c76bad5fce49f315d89
amixrzi <amixrzie@gmail.com>
2026-09-19 15:07:49 +0330

    Implement challenge 04

---
+++ 09-Data-Structures-Operators/22-challenge-04.js
@@ -0,0 +1,14 @@
+'use strict';
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = [...document.querySelector('textarea').value.split('\n')];
+  console.log(text);
+  for (const [t, row] of text.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(t + 1)}`);
+  }
+});
