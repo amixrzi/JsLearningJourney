8452eab7d0a8138f45e3bd0d5b11ca519f69255b
amixrzi <amixrzie@gmail.com>
2026-09-22 23:22:33 +0330

    Implement bind method

---
+++ 10-Functions/06-bind-method.js
@@ -0,0 +1,92 @@
+'use strict';
+'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  booking: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+    );
+    this.booking.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+lufthansa.book(487, 'Amir');
+lufthansa.book(345, 'Mamad');
+console.log(lufthansa);
+
+const euroWings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  booking: [],
+};
+
+const book = lufthansa.book;
+// Dos not work
+// book(23, 'Sara Williams');
+
+// Call method
+book.call(euroWings, 23, 'Sara Williams');
+console.log(euroWings.booking);
+
+book.call(lufthansa, 325, 'Patrik Jane');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss',
+  iataCode: 'LX',
+  booking: [],
+};
+book.call(swiss, 98, 'Patrik Jane');
+console.log(swiss);
+
+// Apply method
+const flightData = [666, 'Red John'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// Bind method
+// book.call(euroWings, 23, 'Sara Williams');
+const bookEW = book.bind(euroWings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(67, 'Tony Stark');
+
+const bookEW23 = book.bind(euroWings, 23);
+bookEW23('Amirhossein Rezaei');
+bookEW23('Patrik Jane');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.byPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.byPlane.bind(lufthansa));
+
+// Partial applications
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 300));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23
+
+console.log(addVAT(100));
+console.log(addVAT(867));
+
+const addTax2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTax2(0.23);
+console.log(addVAT2(110));
