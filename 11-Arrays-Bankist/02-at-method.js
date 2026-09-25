206f258505958c0cb3115776481df715bd7c47b2
amixrzi <amixrzie@gmail.com>
2026-09-25 23:58:56 +0330

    Implement at method

---
+++ 11-Arrays-Bankist/02-at-method.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const arr = [76, 23, 94];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('Amir'.at(0));
